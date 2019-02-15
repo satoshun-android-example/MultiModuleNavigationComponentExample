cbf1fbc390837bed32fb499ffd4ec9122fe34efc
SatoShun <shun.sato1@gmail.com>
2019-02-15 00:14:01 +0000

    use navigation action

--- router/src/main/java/com/github/satoshun/example/feature/router/Router.kt
+++ router/src/main/java/com/github/satoshun/example/feature/router/Router.kt
@@ -2,7 +2,9 @@ package com.github.satoshun.example.feature.router
 
 import android.content.Context
 import androidx.navigation.NavController
+import com.github.satoshun.example.feature.main.MainFragmentDirections
 import com.github.satoshun.example.feature.main.MainModuleRouter
+import com.github.satoshun.example.feature.sub1.Sub1FragmentDirections
 import com.github.satoshun.example.feature.sub1.Sub1ModuleRouter
 import javax.inject.Inject
 
@@ -10,7 +12,7 @@ class MainModuleRouterImpl @Inject constructor(
   private val controller: NavController
 ) : MainModuleRouter {
   override fun routeToSub1(context: Context) {
-    controller.navigate(R.id.nav_sub1_frag)
+    controller.navigate(MainFragmentDirections.mainToSub1())
   }
 }
 
@@ -18,6 +20,6 @@ class Sub1ModuleRouterImpl @Inject constructor(
   private val controller: NavController
 ) : Sub1ModuleRouter {
   override fun routeToMain(context: Context) {
-    controller.navigate(R.id.nav_main_frag)
+    controller.navigate(Sub1FragmentDirections.sub1ToMain())
   }
 }
